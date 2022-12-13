41c534780a6714c9c4559114faec86379e70bb25
AlexCorn999 <alexcorn999@list.ru>
2022-12-13 22:37:43 +0300

    develop/grep 1.0

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// описание флагов
 typedef struct options {
   int b;
   int e;
@@ -17,12 +15,9 @@ void parser(int ARGC, char *ARGV[], opt *options);
 void reader(char *ARGV[], opt *options);
 
 int main(int ARGC, char *ARGV[]) {
-
-  // обнуление флагов
   opt options = {0};
   parser(ARGC, ARGV, &options);
 
-  // проверка идентичных опций
   if (options.b == 1) {
     options.n = 0;
     reader(ARGV, &options);
@@ -43,51 +38,52 @@ void parser(int ARGC, char *ARGV[], opt *options) {
   while ((opt = getopt_long(ARGC, ARGV, "+benstvTE", long_options,
                             &option_index)) != -1) {
     switch (opt) {
-    case 'b':
-      options->b = 1;
-      break;
-    case 'n':
-      options->n = 1;
-      break;
-    case 'e':
-      options->e = 1;
-      options->v = 1;
-      break;
-    case 's':
-      options->s = 1;
-      break;
-    case 't':
-      options->t = 1;
-      options->v = 1;
-      break;
-    case 'v':
-      options->v = 1;
-      break;
-    case 'T':
-      options->t = 1;
-      break;
-    case 'E':
-      options->e = 1;
-      break;
-    default:
-      fprintf(stderr, "usage: cat [-benstuv] [file ...]");
-      exit(1);
+      case 'b':
+        options->b = 1;
+        break;
+      case 'n':
+        options->n = 1;
+        break;
+      case 'e':
+        options->e = 1;
+        options->v = 1;
+        break;
+      case 's':
+        options->s = 1;
+        break;
+      case 't':
+        options->t = 1;
+        options->v = 1;
+        break;
+      case 'v':
+        options->v = 1;
+        break;
+      case 'T':
+        options->t = 1;
+        break;
+      case 'E':
+        options->e = 1;
+        break;
+      default:
+        fprintf(stderr, "usage: cat [-benstuv] [file ...]");
+        exit(1);
     }
   }
 }
 
 void reader(char *ARGV[], opt *options) {
-  FILE *f = fopen(ARGV[optind], "r");
-  if (f) {
-    int prev = '\n';
-    int q = 0;
-    int flag_127 = 0;
-    int current;
-    int str_count = 1;
-    int p = 0;
+  while (ARGV[optind]) {
+    FILE *f = fopen(ARGV[optind], "r");
 
-    while ((current = fgetc(f)) != EOF) {
+    if (f) {
+      int prev = '\n';
+      int current;
+      int flag_127 = 0;
+      int q = 0;
+      int p = 0;
+      int str_count = 1;
 
+      while ((current = fgetc(f)) != EOF) {
         if (options->s == 1) {
           if (prev == '\n') {
             char next = fgetc(f);
@@ -98,84 +94,76 @@ void reader(char *ARGV[], opt *options) {
           } else {
           }
         }
-        
-        
-      if (options->b == 1) {
-        if (current != '\n' && prev == '\n') {
+
+        if (options->b == 1) {
+          if (current != '\n' && prev == '\n') {
             printf("%6d\t", str_count++);
+          }
         }
-      }
-        
-      if (options->t == 1) {
-                if (current == '\t') {
-                  current = 'I';
-                  p= 1;
-                }
-              }
 
-
-
-      if (options->v == 1) {
-        if ((current >= 0 && current <= 31 && current != '\n' &&
-             current != '\t') ||
-            current == 127 || current > 127) {
-          if (current >= 0 && current <= 31 && current != '\n' &&
-              current != '\t') {
-            current = current + 64;
-            q = 1;
+        if (options->t == 1) {
+          if (current == '\t') {
+            current = 'I';
+            p = 1;
           }
-          if (current > 127 && current <= 160) {
-            current = (current & 31) + 64;
-            flag_127 = 1;
-          }
-          if (current == 127) {
-            current = '?';
-            q = 1;
-          }
-
-        } else {
-          q = 0;
-          flag_127 = 0;
         }
-      }
-        
-        
-        
-        if (options->n == 1 ) {
+
+        if (options->v == 1) {
+          if ((current >= 0 && current <= 31 && current != '\n' &&
+               current != '\t') ||
+              current == 127 || current > 127) {
+            if (current >= 0 && current <= 31 && current != '\n' &&
+                current != '\t') {
+              current = current + 64;
+              q = 1;
+            }
+            if (current > 127 && current <= 160) {
+              current = (current & 31) + 64;
+              flag_127 = 1;
+            }
+            if (current == 127) {
+              current = '?';
+              q = 1;
+            }
+
+          } else {
+            q = 0;
+            flag_127 = 0;
+          }
+        }
+
+        if (options->n == 1) {
           if (prev == '\n') {
             printf("%6d\t", str_count++);
           } else {
           }
         }
-        
 
         if (options->e == 1) {
-          if (prev == '\n' && current == '\n' && (options->n == 0 || options->b == 1) && (options->s == 0 || options->t == 0 || options->v == 0)) {
-            printf("      	$");
-          } else if (current == '\n') {
-            printf("$");
+          if (current == '\n') {
+            fprintf(stdout, "$");
           }
         }
-        
-        
-      if (p == 1) {
-        printf("^%c", current);
-        prev = current;
-        p = 0;
-      } else if (q == 1) {
-        printf("^%c", current);
-        prev = current;
-      } else if (flag_127 == 1) {
-        printf("M-^%c", current);
-        prev = current;
-      } else {
-        prev = current;
-        putchar(current);
-      }
-    }
-  } else {
-    printf("cat: %s: No such file or directory", ARGV[optind]);
-  }
 
-  fclose(f);
+        if (p == 1) {
+          printf("^%c", current);
+          prev = current;
+          p = 0;
+        } else if (q == 1) {
+          printf("^%c", current);
+          prev = current;
+        } else if (flag_127 == 1) {
+          printf("M-^%c", current);
+          prev = current;
+        } else {
+          prev = current;
+          putchar(current);
+        }
+      }
+    } else {
+      fprintf(stderr, "cat: %s: No such file or directory\n", ARGV[optind]);
+    }
+    fclose(f);
+    optind++;
+  }
 }
--- src/grep/grep.c
+++ src/grep/grep.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 #include <getopt.h>
-#include <stdlib.h>
 #include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 typedef struct options {
@@ -21,126 +21,123 @@ void parser(int ARGC, char *ARGV[], opt *options);
 void reader(char *ARGV[], opt *options);
 
 int main(int ARGC, char *ARGV[]) {
-    opt options = {0};
-    parser(ARGC, ARGV, &options);
-    reader(ARGV, &options);
+  opt options = {0};
+  parser(ARGC, ARGV, &options);
+  reader(ARGV, &options);
 }
 
 void parser(int ARGC, char *ARGV[], opt *options) {
   int opt;
   while ((opt = getopt(ARGC, ARGV, "eivclnhsfo")) != -1) {
     switch (opt) {
-        case 'e':
-          options->e = 1;
-          break;
-        case 'i':
-          options->i = 1;
-          break;
-        case 'v':
-          options->v = 1;
-          break;
-        case 'c':
-          options->c = 1;
-          break;
-        case 'l':
-          options->l = 1;
-          break;
-        case 'n':
-          options->n = 1;
-          break;
-        case 'h':
-          options->h = 1;
-          break;
-        case 's':
-          options->s = 1;
-          break;
-        case 'f':
-          options->f = 1;
-          break;
-        case 'o':
-          options->o = 1;
-          break;
-        default:
-          fprintf(stderr, "usage: grep [-eivclnhsfo] [file ...]");
-          exit(1);
+      case 'e':
+        options->e = 1;
+        break;
+      case 'i':
+        options->i = 1;
+        break;
+      case 'v':
+        options->v = 1;
+        break;
+      case 'c':
+        options->c = 1;
+        break;
+      case 'l':
+        options->l = 1;
+        break;
+      case 'n':
+        options->n = 1;
+        break;
+      case 'h':
+        options->h = 1;
+        break;
+      case 's':
+        options->s = 1;
+        break;
+      case 'f':
+        options->f = 1;
+        break;
+      case 'o':
+        options->o = 1;
+        break;
+      default:
+        fprintf(stderr, "usage: grep [-eivclnhsfo] [file ...]");
+        exit(1);
     }
   }
 }
 
-
 void reader(char *ARGV[], opt *options) {
-    char search_string [1024];
-    strcpy(search_string, ARGV[optind]);
-    regex_t regex;
-    int regflag = 0;
-    int red;
-    char * tmp_line = NULL;
-    size_t len = 0;
-    int compare = 0;
-    int success = 0;
-    int str_count = 0;
-    int over = 0;
-    int find_success = 0;
-    
-    FILE *f;
-    f = fopen(ARGV[optind + 1], "r");
-    
+  char search_string[1024];
+  strcpy(search_string, ARGV[optind]);
+  regex_t regex;
+  int regflag = 0;
+  int red = 0;
+  char *tmp_line = NULL;
+  size_t len = 0;
+  int compare = 0;
+  int success = 0;
+  int str_count = 0;
+  int over = 0;
+  int find_success = 0;
+
+  optind++;
+  while (ARGV[optind]) {
+    FILE *f = fopen(ARGV[optind], "r");
+
     if (f) {
-        if (options->i == 1) {
-            regflag = REG_ICASE;
-        }
-    
-        if (options->v == 1) {
-            compare = REG_NOMATCH;
-        }
-    
-        if (options->l == 1) {
-            over = 1;
-            
-        }
-    
-    regcomp(&regex, search_string, regflag);
-    while (red != EOF) {
+      if (options->i == 1) {
+        regflag = REG_ICASE;
+      }
+
+      if (options->v == 1) {
+        compare = REG_NOMATCH;
+      }
+
+      if (options->l == 1) {
+        over = 1;
+      }
+
+      regcomp(&regex, search_string, regflag);
+      while (red != EOF) {
         red = getline(&tmp_line, &len, f);
         if (tmp_line && red != EOF) {
-            str_count++;
-            success = regexec(&regex, tmp_line, 0, NULL, 0);
-            find_success++;
-            if (success == compare && over != 1 && options->c != 1) {
-                
-                if (options->n == 1) {
-                    printf("%d:", str_count);
-                }
-                
-                if (options->h != 1) {
-                    printf("%s", ARGV[optind + 1]);
-                }
-                
-                
-                printf("%s", tmp_line);
-                
+          str_count++;
+          success = regexec(&regex, tmp_line, 0, NULL, 0);
+          find_success++;
+          if (success == compare && over != 1 && options->c != 1) {
+            if (options->n == 1) {
+              printf("%d:", str_count);
             }
-            
-            if (success == compare && over == 1) {
-                printf("%s", ARGV[optind + 1]);
-                break;
-            }
-        }
-    }
-        
-        free(tmp_line);
-        regfree(&regex);
-        
-        if (options->c == 1) {
-            printf("%s:", ARGV[optind + 1]);
-            printf("%d", find_success);
-        }
-        
-        
-        
-    } else {
-              printf("No such file or directory");
-    }
-    
-    }
 
+            if (options->h != 1) {
+              printf("%s", ARGV[optind + 1]);
+            }
+
+            printf("%s", tmp_line);
+          }
+
+          if (success == compare && over == 1) {
+            printf("%s", ARGV[optind + 1]);
+            break;
+          }
+        }
+      }
+
+      free(tmp_line);
+      regfree(&regex);
+
+      if (options->c == 1) {
+        printf("%s:", ARGV[optind + 1]);
+        printf("%d", find_success);
+      }
+
+    } else {
+      fprintf(stderr, "grep: %s: No such file or directory\n",
+              ARGV[optind + 2]);
+      break;
+    }
+    fclose(f);
+    optind++;
+  }
+}
